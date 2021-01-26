662bb3585968231fd6dab5489e088a11a0e1b093
Serezha <serezhaolshan@gmail.com>
2021-01-26 02:50:30 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reversed (n) {
+//   return Number((''+n).split('').reverse().join(''));
+//    var arr = (''+n).split('');
+
+//    if(arr[0] == '-'){
+//        arr.splice(0,0);
+//        arr.reverse();
+//    } else{
+//        arr.reverse();
+//    }
+
+//    return Number(arr);
+
+return parseInt(n.toString().split("-").map(x=> x.split("").reverse().join("")).join(""));
 }
